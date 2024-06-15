6f50aff3ce53dbdfdbbf14f1127d738d415a4d2f
Becky <elizabethbecky17@gmail.com>
2024-06-15 23:45:45 +0100

    Added loin page responsivity

--- TechBlaze/src/pages/registeration/partials/LoginBento.jsx
+++ TechBlaze/src/pages/registeration/partials/LoginBento.jsx
@@ -4,15 +4,15 @@ import bentoTab from "../../../assets/images/login-tab.svg";
 const LoginBento = () => {
   return (
     <section>
-      <div className=" relative w-full max-h-full overflow-hidden">
+      <div className=" relative w-full max-h-full overflow-hidden hidden lg:block">
         <img
           src={bentoImg}
-          alt="bento grid"
+          alt="mountain view"
           className="h-full w-full object-contain"
         />
       </div>
-      <div>
-        <img src={bentoTab} alt="mountain view" />
+      <div className="relative w-full mt- lg:hidden ">
+        <img src={bentoTab} alt="mountain view"  />
       </div>
     </section>
   );
