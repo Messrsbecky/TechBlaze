bc5107e41d9115902b0e5913cfd8a782cbaf3f16
Becky <elizabethbecky17@gmail.com>
2024-06-18 00:29:16 +0100

    Added links to Navbar

--- TechBlaze/src/components/Nav.jsx
+++ TechBlaze/src/components/Nav.jsx
@@ -1,21 +1,32 @@
-import menu from "../assets/icons/Menu.svg"
-import nomadHavenLogo from "../assets/icons/NOMADHAVEN teal.svg"
+import { Link, NavLink } from "react-router-dom";
+import menu from "../assets/icons/Menu.svg";
+import nomadHavenLogo from "../assets/icons/NOMADHAVEN teal.svg";
 const Navbar = () => {
   return (
-    <div className="bg-white opacity-[0.8] flex justify-between items-center border-2 border-pink-500 pt-9 px-4 md:px-20 pb-5 nav">
-      <div className="border-purple-400 border-2 ">
-        <img src={nomadHavenLogo} alt="NomadHaven logo " />
+    <header className="bg-white opacity-[0.8] flex justify-between items-center pt-9 px-4 md:px-20 pb-5 nav">
+      <div className="">
+        <NavLink to="/">
+          <img src={nomadHavenLogo} alt="NomadHaven logo " />
+        </NavLink>
       </div>
-      <div className=" justify-between border-2 border-red-600 w-[72%] hidden lg:flex">
-        <div className="border-2 border-yellow-400 flex justify-around w-1/2">
-          <ul className="flex gap-2 items-center justify-around border-2 border-green-500 w-full footer-ul">
-            <li>How to use</li>
-            <li>Hotspots</li>
-            <li>Features</li>
-            <li>Events</li>
+      <div className=" justify-between w-[72%] hidden lg:flex">
+        <div className=" flex justify-around w-1/2">
+          <ul className="flex gap-2 items-center justify-around w-full footer-ul">
+            <NavLink to="how to use">
+              <li>How to use</li>
+            </NavLink>
+            <NavLink to="hotspots">
+              <li>Hotspots</li>
+            </NavLink>
+            <NavLink to="features">
+              <li>Features</li>
+            </NavLink>
+            <NavLink to="events">
+              <li>Events</li>
+            </NavLink>
           </ul>
         </div>
-        <div className="border-2 border-blue-500 flex ml-auto gap-2 w-fit items-center justify-center">
+        <div className="flex ml-auto gap-2 w-fit items-center justify-center">
           <div>
             <button className="secondaryBtn text-Black">Login</button>
           </div>
@@ -27,7 +38,7 @@ const Navbar = () => {
       <div className="lg:hidden">
         <img src={menu} alt="menu icon" />
       </div>
-    </div>
+    </header>
   );
 };
 
